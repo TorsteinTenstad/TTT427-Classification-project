a6e599681470ce99474bbd45e3ba2c7d8642edec
tobiaskr <tobiaskr@stud.ntnu.no>
2020-03-22 11:34:32 +0100

    Histogram ok

--- iris_project/main.py
+++ iris_project/main.py
@@ -1,6 +1,7 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
-np.set_printoptions(precision=2)
+plt.style.use('seaborn-whitegrid')
 
 def sigmoid(x, W):
     return 1 / (1 + np.e ** (-np.matmul(W, x)))
@@ -125,11 +126,48 @@ def task1():
     print(confusion_matrix_test + confusion_matrix_training)
 
 def task2():
-    return 0
+    # Load data from csv-file
+    data_class1 = np.loadtxt(open("Data/class_1", "rb"), delimiter=",", skiprows=1)
+    data_class2 = np.loadtxt(open("Data/class_2", "rb"), delimiter=",", skiprows=1)
+    data_class3 = np.loadtxt(open("Data/class_3", "rb"), delimiter=",", skiprows=1)
+
+    # Plot histogram
+    fig, axs = plt.subplots(3, 4, sharey=True, sharex='col', tight_layout=True)
+    n_bins = 15
+
+    #Class 1
+    axs[0,0].hist(data_class1[:, 0], bins = n_bins)
+    axs[0,1].hist(data_class1[:, 1], bins = n_bins)
+    axs[0,2].hist(data_class1[:, 2], bins = n_bins)
+    axs[0,3].hist(data_class1[:, 3], bins = n_bins)
+
+    #Class 2
+    axs[1, 0].hist(data_class2[:, 0], bins = n_bins)
+    axs[1, 1].hist(data_class2[:, 1], bins = n_bins)
+    axs[1, 2].hist(data_class2[:, 2], bins = n_bins)
+    axs[1, 3].hist(data_class2[:, 3], bins = n_bins)
+
+    # Class 3
+    axs[2, 0].hist(data_class3[:, 0], bins = n_bins)
+    axs[2, 1].hist(data_class3[:, 1], bins = n_bins)
+    axs[2, 2].hist(data_class3[:, 2], bins = n_bins)
+    axs[2, 3].hist(data_class3[:, 3], bins = n_bins)
+
+    axs[0, 0].set_ylabel('Class 1')
+    axs[1, 0].set_ylabel('Class 2')
+    axs[2, 0].set_ylabel('Class 3')
+
+    axs[0, 0].set_title('Sepal length')
+    axs[0, 1].set_title('Sepal width')
+    axs[0, 2].set_title('Petal length')
+    axs[0, 3].set_title('Petal width')
+
+    plt.show()
+
 
 def main():
     print("======  task1  ======")
-    task1()
+    #task1()
 
     print("======  task2  ======")
     task2()
